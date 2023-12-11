c88d61dbd161e81ff7fb7861211002a3e70fae43
michelmnds <m1tchofc@gmail.com>
2023-12-11 15:23:29 +0000

    Solved Lab

--- src/components/CreditCard/index.jsx
+++ src/components/CreditCard/index.jsx
@@ -1,4 +1,5 @@
 import visa from "../../assets/images/visa.png";
+import masterCard from "../../assets/images/master-card.svg";
 import "./style.css";
 
 export const CreditCard = ({
@@ -18,6 +19,31 @@ export const CreditCard = ({
         style={{ backgroundColor: bgColor, color: color }}
       >
         <img style={{ width: 50 }} src={visa} alt="" />
+
+        <div className="numContainer">
+          <span className="num">
+            .... .... .... {number.toString().slice(-4)}
+          </span>
+        </div>
+
+        <div className="expContainer">
+          <span>
+            Expires {expirationMonth}/{expirationYear.toString().slice(-2)}
+          </span>
+
+          <span className="bank">{bank}</span>
+        </div>
+
+        <div className="owner">{owner}</div>
+      </div>
+    );
+  } else if (type === "Master Card") {
+    return (
+      <div
+        className="creditContainer"
+        style={{ backgroundColor: bgColor, color: color }}
+      >
+        <img style={{ width: 50 }} src={masterCard} alt="" />
 
         <div className="numContainer">
           <span className="num">
